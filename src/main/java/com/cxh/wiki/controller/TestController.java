8938933f139839b6c852dcb8f8e1df7c8284f10f
cxh <763591761@qq.com>
2021-08-12 20:34:03 +0800

    2-7 增加hello接口测试test.http

--- src/main/java/com/cxh/wiki/controller/TestController.java
+++ src/main/java/com/cxh/wiki/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.cxh.wiki.controller;
 
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
@@ -11,7 +13,6 @@ public class TestController {
      *
      * /user?id=1
      * /user/1
-     * @return
      */
 //    @GetMapping("/hello")
 //    @PostMapping
@@ -22,5 +23,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello(){
         return "Hello World!";
+    }
+
+    @PostMapping("/hello/post")
+    public String hello(String name){
+        return "Hello World! Post, " + name;
     }
 }
