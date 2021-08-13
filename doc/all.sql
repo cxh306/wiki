39618e3f0564474fae04453bb3ce44464f4295c2
cxh <763591761@qq.com>
2021-08-13 14:37:26 +0800

    2-3 增加了all.sql脚本

--- doc/all.sql
+++ doc/all.sql
@@ -1,6 +1,6 @@
 drop table if exists `test`;
 create table `test` (
-     `id` bigint not null comment 'id',
+    `id` bigint not null comment 'id',
      `name` varchar(50) comment '名称',
      `password` varchar(50) comment '密码',
       primary key (`id`)
