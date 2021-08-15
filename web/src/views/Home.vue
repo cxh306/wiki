3a5f2e15b5e9e62ddad6899795f98be4dba55cc4
cxh <763591761@qq.com>
2021-08-15 20:59:47 +0800

    5-2 使用axios调用后端接口/ebook/list

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -71,8 +71,16 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 // @ is an alias to /src
+import axios from "axios";
 
 export default defineComponent({
   name: 'Home',
+  setup(){
+    console.log("setup");
+    //response =>{}  ====function(response){}
+    axios.get("http://localhost:8088/ebook/list?name=Spring").then(response =>{
+      console.log(response);
+    })
+  }
 });
 </script>
