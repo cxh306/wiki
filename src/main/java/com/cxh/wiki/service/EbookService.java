adb0d87384bd2dbe409d1d48bfc4767f3061ec7d
cxh <763591761@qq.com>
2021-08-16 13:50:11 +0800

    5-4 电子书布局调整 一行显示三列电子书

--- src/main/java/com/cxh/wiki/service/EbookService.java
+++ src/main/java/com/cxh/wiki/service/EbookService.java
@@ -7,6 +7,8 @@ import com.cxh.wiki.req.EbookReq;
 import com.cxh.wiki.resp.EbookResp;
 import com.cxh.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import javax.annotation.Resource;
 import java.util.List;
@@ -20,7 +22,10 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%"+req.getName()+"%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%"+req.getName()+"%");
+        }
+
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);//数据库字段对象Ebook
 
 //        List<EbookResp> respList = new ArrayList<>();//一些敏感字段不希望返回给前端,故创建排除敏感字段的类EbookResq
--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -63,35 +63,22 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-        <template #footer>
-          <div>
-            <b>ant design vue</b>
-            footer part
-          </div>
-        </template>
-        <template #renderItem="{ item }">
-          <a-list-item key="item.title">
+      <a-list item-layout="vertical" size="large" :grid="{ gutter: 20, column: 3 }" :data-source="ebooks">
+        <template  #renderItem="{ item }">
+          <a-list-item key="item.name">
             <template #actions>
-          <span v-for="{ type, text } in actions" :key="type">
-            <component v-bind:is="type" style="margin-right: 8px" />
-            {{ text }}
-          </span>
+              <span v-for="{ type, text } in actions" :key="type">
+                <component v-bind:is="type" style="margin-right: 8px" />
+                {{ text }}
+              </span>
             </template>
-            <template #extra>
-              <img
-                  width="272"
-                  alt="logo"
-                  src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-              />
-            </template>
+
             <a-list-item-meta :description="item.description">
               <template #title>
-                <a :href="item.href">{{ item.title }}</a>
+                <a :href="item.href">{{ item.name }}</a>
               </template>
-              <template #avatar><a-avatar :src="item.avatar" /></template>
+              <template #avatar><a-avatar :src="item.cover" /></template>
             </a-list-item-meta>
-            {{ item.content }}
           </a-list-item>
         </template>
       </a-list>
@@ -100,9 +87,9 @@
 </template>
 
 <script lang="ts">
-import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
-import { defineComponent } from 'vue';
-
+import { defineComponent,onMounted, ref, reactive, toRef} from 'vue';
+import axios from 'axios';
+// @ is an alias to /src
 const listData: Record<string, string>[] = [];
 
 for (let i = 0; i < 23; i++) {
@@ -118,12 +105,20 @@ for (let i = 0; i < 23; i++) {
 }
 
 export default defineComponent({
-  components: {
-    StarOutlined,
-    LikeOutlined,
-    MessageOutlined,
-  },
-  setup() {
+  name: 'Home',
+  setup(){
+    console.log("setup");
+    const ebooks  = ref();
+    const ebooks1 = reactive({books: []});
+    onMounted(() => {
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring").then((response) =>{
+        const data = response.data;
+        ebooks.value=data.content;
+        ebooks1.books = data.content;
+        console.log(response);
+      })
+    })
     const pagination = {
       onChange: (page: number) => {
         console.log(page);
@@ -136,10 +131,12 @@ export default defineComponent({
       { type: 'MessageOutlined', text: '2' },
     ];
     return {
+      ebooks,
+      ebooks2: toRef(ebooks1, "books"),
       listData,
       pagination,
       actions,
     };
-  },
+  }
 });
 </script>
