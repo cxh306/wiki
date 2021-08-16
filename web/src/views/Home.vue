81ef2f90878e79f0e515526e13ffdea7c74fc567
cxh <763591761@qq.com>
2021-08-16 14:38:49 +0800

    5-6 配置axios拦截器打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -111,12 +111,10 @@ export default defineComponent({
     const ebooks  = ref();
     const ebooks1 = reactive({books: []});
     onMounted(() => {
-      console.log("onMounted22");
       axios.get("/ebook/list").then((response) =>{
         const data = response.data;
         ebooks.value=data.content;
         ebooks1.books = data.content;
-        console.log(response);
       })
     })
     const pagination = {
