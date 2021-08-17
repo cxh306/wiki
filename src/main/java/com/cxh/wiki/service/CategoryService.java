de3aa04f2955b050bcb7e850df9e6fd9e8fb2e00
cxh <763591761@qq.com>
2021-08-17 15:31:05 +0800

    7-3 完成分类管理功能的增删改查功能(从电子书管理复制)

---
+++ src/main/java/com/cxh/wiki/service/CategoryService.java
@@ -0,0 +1,84 @@
+package com.cxh.wiki.service;
+
+import com.cxh.wiki.domain.Category;
+import com.cxh.wiki.domain.CategoryExample;
+import com.cxh.wiki.mapper.CategoryMapper;
+import com.cxh.wiki.req.CategoryQueryReq;
+import com.cxh.wiki.req.CategorySaveReq;
+import com.cxh.wiki.resp.CategoryQueryResp;
+import com.cxh.wiki.resp.PageResp;
+import com.cxh.wiki.util.CopyUtil;
+import com.cxh.wiki.util.SnowFlake;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CategoryService.class);
+
+    @Resource
+    private CategoryMapper categoryMapper;
+
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<CategoryQueryResp> list(CategoryQueryReq req) {
+        CategoryExample categoryExample = new CategoryExample();
+        CategoryExample.Criteria criteria = categoryExample.createCriteria();
+
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Category> categoryList = categoryMapper.selectByExample(categoryExample);
+
+        PageInfo<Category> pageInfo = new PageInfo<>(categoryList);
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
+        // List<CategoryResp> respList = new ArrayList<>();
+        // for (Category category : categoryList) {
+        //     // CategoryResp categoryResp = new CategoryResp();
+        //     // BeanUtils.copyProperties(category, categoryResp);
+        //     // 对象复制
+        //     CategoryResp categoryResp = CopyUtil.copy(category, CategoryResp.class);
+        //
+        //     respList.add(categoryResp);
+        // }
+
+        // 列表复制
+        List<CategoryQueryResp> list = CopyUtil.copyList(categoryList, CategoryQueryResp.class);
+
+        PageResp<CategoryQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+
+        return pageResp;
+    }
+
+    /**
+     * 保存
+     */
+    public void save(CategorySaveReq req) {
+        Category category = CopyUtil.copy(req, Category.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            category.setId(snowFlake.nextId());
+            categoryMapper.insert(category);
+        } else {
+            // 更新
+            categoryMapper.updateByPrimaryKey(category);
+        }
+    }
+
+    public void delete(Long id) {
+        categoryMapper.deleteByPrimaryKey(id);
+    }
+
+}
+
